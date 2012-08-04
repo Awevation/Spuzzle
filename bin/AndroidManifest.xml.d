60a2ef209fab168b51022422a114a01dd209e147
Milo Mordaunt <milomord@gmail.com>
2012-08-04 18:36:06 +0100

    cleaned

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/milo/Projects/Android/Spuzzle/bin/AndroidManifest.xml : \
-/home/milo/Projects/Android/Spuzzle/AndroidManifest.xml \
--- bin/Spuzzle-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/milo/Projects/Android/Spuzzle/bin/Spuzzle-debug-unaligned.apk : \
-/home/milo/Projects/Android/Spuzzle/bin/Spuzzle.ap_ \
-/home/milo/Projects/Android/Spuzzle/bin/classes.dex \
-/opt/android-sdk/tools/support/annotations.jar \
