a790e018041ef1db97fbb4c3eb863f9a8c4c70f5
Milo Mordaunt <milomord@gmail.com>
2012-10-15 18:53:25 +0100

    made the script 100x more awesome. ie, it's multipurpose now.

--- buildLinux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Building..."; 
-ant debug;
-echo ""; 
-echo "";
-echo "Installing....";
-adb install -r bin/Spuzzle-debug.apk;
--- run.sh
+++
@@ -1,4 +0,0 @@
-#Script to start the Andoid app
-
-#'am' is found on most/all devices.. I hope..
-am start -a android.intent.action.MAIN -n com.awevation.spuzzle/.StartActivity
